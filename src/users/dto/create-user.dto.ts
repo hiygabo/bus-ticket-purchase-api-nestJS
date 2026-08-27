357b761255d3183991c5e83d5461cbef345c5d89
hiygabo <gaboandiaalave@gmail.com>
2026-08-26 22:17:50 -0400

    Add(dto) add create user dto - create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail({}, { message: 'Must be a valid email' })
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'Password must have almost 6 characters ' })
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  role?: string;
+}
