eb8ce6e5d8d39305dc4ee1b823f4f2e3a1e05232
hiygabo <gaboandiaalave@gmail.com>
2026-08-26 22:36:12 -0400

    Add(controller) add user controller - users.controller.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
+  async createUser(createUserDto: CreateUserDto) {
+    const userExists = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    if (userExists) {
+      throw new BadRequestException('This email is already register');
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const newUser = this.userRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.userRepository.save(newUser);
+  }
+  async findByEmail(email: string) {
+    return this.userRepository.findOne({ where: { email } });
   }
 }
