8b835ff156cb0066638335a254a324a4d327cff1
hiygabo <gaboandiaalave@gmail.com>
2026-08-30 19:40:27 -0400

    Feat(.env.example) add env example and change on docker compose - docker-compose.yml, .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: Number(process.env.PORT),
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
